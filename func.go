f932d57f93629a29c727fd58ce998369da10117c
dips <dipeshshrestha910@gmail.com>
2025-01-20 13:30:41 +0545

    multiple return types with callback as well

--- func.go
+++ func.go
@@ -1,23 +1,12 @@
 package main
-import "fmt"
 
-//in go we also use functions to define the code and the main function is the entry point of the code similar to js 
+import (
+	"fmt"
+	"strings"
+)
+
+//in go we also use functions to define the code and the main function is the entry point of the code similar to js
 //function that isn't called isn't a error in go
-func main(){
-//callign a function is also similar 
-  greet("dips");
-
-  bye("dips");
-
-  //we can also pass a function as a parameter to another function callback function basically
-  cycleNames([]string{"dips","dave","david"},greet)
-
-//when calling a func with return type we need to store it in vallue for sideeffect to be seen
-  r1:=radius(5);
-  fmt.Println(r1);
-
-
-}
 //Declaring a function is very similar to js with func keyword
 func greet(u string){
   fmt.Printf("hello %v\n",u)
@@ -37,4 +26,42 @@ func radius(r int)int{
   return r*r; 
 
 }
+//now lets make a name separator with initial name and last name 
+//since the return type are multiple we have to define return type that much times 
+func nameSaparator(name string)(string,string){
+  fmt.Println("the full name is",name);
+  //splitting the name into first and last name
+  nameSplit:= strings.Split(name, " ")
+  if len(nameSplit)>1{
 
+    return nameSplit[0],nameSplit[1];
+  }
+  return name, ""
+
+
+  }
+
+
+
+
+
+
+
+func main(){
+  first,last:=nameSaparator("dipesh shrestha");
+
+  //callign a function is also similar 
+  greet("dips");
+
+  bye("dips");
+
+  //we can also pass a function as a parameter to another function callback function basically
+  cycleNames([]string{"dips","dave","david"},greet)
+
+  //when calling a func with return type we need to store it in vallue for sideeffect to be seen
+  r1:=radius(5);
+  fmt.Println(r1);
+
+  fmt.Println(first,last);
+
+}
